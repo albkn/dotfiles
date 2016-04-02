3f888a8ba1e26eb00576bb1322cd7ea4bbbe5783
Albert Kurniawan <kurniawan.albert19@gmail.com>
2016-04-03 02:18:57 +1100

    moved this whole thing to a new folder with contents to be symlinked

---
+++ dotfiles.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+base_dir=".dotfiles"
+
+if [ "$1" = "link" ]; then
+  ln -s "$base_dir"/.bash_custom ~/.bash_custom
+  ln -s "$base_dir"/.tmux.conf ~/.tmux.conf
+  ln -s "$base_dir"/.vimrc ~/.vimrc
+  ln -s "$base_dir"/.Xresources ~/.Xresources
+elif [ "$1" = "unlink" ]; then
+  test -h ~/.bash_custom && rm ~/.bash_custom
+  test -h ~/.tmux.conf && rm ~/.tmux.conf
+  test -h ~/.vimrc && rm ~/.vimrc
+  test -h ~/.Xresources && rm ~/.Xresources
+fi
