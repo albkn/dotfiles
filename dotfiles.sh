b31fe17f0e36f200e9deb2fff26f068bf52a06d2
Albert Kurniawan <kurniawan.albert19@gmail.com>
2016-04-04 08:58:25 +1000

    Added .gitconfig

--- dotfiles.sh
+++ dotfiles.sh
@@ -3,11 +3,13 @@
 base_dir=".dotfiles"
 
 if [ "$1" = "link" ]; then
+  ln -s "$base_dir"/.gitconfig ~/.gitconfig
   ln -s "$base_dir"/.bash_custom ~/.bash_custom
   ln -s "$base_dir"/.tmux.conf ~/.tmux.conf
   ln -s "$base_dir"/.vimrc ~/.vimrc
   ln -s "$base_dir"/.Xresources ~/.Xresources
 elif [ "$1" = "unlink" ]; then
+  test -h ~/.gitconfig && rm ~/.gitconfig
   test -h ~/.bash_custom && rm ~/.bash_custom
   test -h ~/.tmux.conf && rm ~/.tmux.conf
   test -h ~/.vimrc && rm ~/.vimrc
